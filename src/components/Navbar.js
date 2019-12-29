88e5461d1b7d649cd80ea78bc57ddb23fb71b2f3
hi-joel <vanbodegraven.joel@gmail.com>
2019-12-29 12:08:46 +0100

    logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,11 +82,7 @@ const Navbar = class extends React.Component {
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
                 target="_blank"
                 rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+              ></a>
             </div>
           </div>
         </div>
